9c086d189c349c869497ee1189f9b547f16d7012
Diego Queiroz <queiroz.diego@gmail.com>
2012-05-04 17:02:56 -0300

    Módulo de download das cotações em arquivo único (demo).

---
+++ demo_downloadCSV_AllInOne.py
@@ -0,0 +1,36 @@
+'''
+Created on May 4, 2012
+
+@author: "DiegoQueiroz"
+'''
+
+from fundo import Fundo
+from datetime import date, timedelta
+from csv import writer as csvwriter
+
+if __name__ == '__main__':
+
+    csv_delimiter  = ';'
+    quote_char     = '"'
+    interval       = 20 # years
+    
+    # Last 20 years interval
+    iniDate = date.today() - timedelta(days= 365 * interval)
+    endDate = date.today()
+
+    with open('FUNDOS.csv','wb') as f:
+
+        arqcsv = csvwriter(f, delimiter=';',quotechar='"')
+
+        for idfundo in Fundo.FUNDS:
+        
+            fundoatual = Fundo(idfundo)
+            fundoatual.updatePrices(iniDate, endDate)
+            
+            data = [ ( [idfundo, x] + y ) for x,y in fundoatual.prices.items() ]
+            data = sorted(data,key=lambda x: x[1])
+            
+            arqcsv.writerows(data)
+                
+            print("Download %s successful!" % ( idfundo ))
+        
