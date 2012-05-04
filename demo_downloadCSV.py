971c0507aadc359a9fd997fa73fb160172c71ab5
Diego Queiroz <queiroz.diego@gmail.com>
2012-05-04 16:24:28 -0300

    Módulo de download das cotações (demo).

---
+++ demo_downloadCSV.py
@@ -0,0 +1,34 @@
+'''
+Created on May 4, 2012
+
+@author: "DiegoQueiroz"
+'''
+
+from fundo import Fundo
+from datetime import date, timedelta
+from csv import writer as csvwriter
+
+if __name__ == '__main__':
+
+    csv_delimiter  = ';'
+    quote_char     = '"'
+    interval       = 20 # years
+    
+    # Last 20 years interval
+    iniDate = date.today() - timedelta(days= 365 * interval)
+    endDate = date.today()
+
+    for idfundo in Fundo.FUNDS:
+        
+        fundoatual = Fundo(idfundo)
+        fundoatual.updatePrices(iniDate, endDate)
+        
+        data = [ ( [x] + y ) for x,y in fundoatual.prices.items() ]
+        data = sorted(data,key=lambda x: x[0])
+        
+        with open(idfundo.replace(' ','_') + '.csv','wb') as f:
+            arqcsv = csvwriter(f, delimiter=';',quotechar='"')
+            arqcsv.writerows(data)
+            
+        print("Download %s successful!" % ( idfundo ))
+        
