3b0c41192ea45efffeea738581ff1da1b0f96247
Diego Queiroz <queiroz.diego@gmail.com>
2012-05-06 04:00:42 -0300

    Módulo de download das cotações em arquivo único para o Quicken (demo).

---
+++ demo_downloadQuicken.py
@@ -0,0 +1,41 @@
+'''
+Created on May 6, 2012
+
+@author: "DiegoQueiroz"
+'''
+
+from fundo import Fundo
+from datetime import date, timedelta
+from csv import writer as csvwriter
+
+# Output file format:
+#
+#   QUOTE , DATE , VALUE, VOLUME
+
+
+if __name__ == '__main__':
+
+    csv_delimiter  = ','
+    quote_char     = '"'
+    interval       = 20 # years
+    
+    # Last 20 years interval
+    iniDate = date.today() - timedelta(days= 365 * interval)
+    endDate = date.today()
+
+    with open('FUNDOS_GF_QUICKEN.csv','wb') as f:
+
+        arqcsv = csvwriter(f, delimiter=csv_delimiter,quotechar=quote_char)
+
+        for idfundo in Fundo.FUNDS:
+        
+            fundoatual = Fundo(idfundo)
+            fundoatual.updatePrices(iniDate, endDate)            
+            
+            data = [ ( [ 'GF_' + str(Fundo.FUNDS[idfundo]), qdate.strftime("%d/%m/%Y"), qvalue, qvolume] )
+                    for qdate,(qvalue,qvolume) in fundoatual.prices.items() ]
+            
+            arqcsv.writerows(data)
+                
+            print("Download %s successful!" % ( idfundo ))
+        
